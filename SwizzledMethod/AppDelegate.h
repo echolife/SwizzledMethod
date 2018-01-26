1a6b7c4ab34d77f6ccb6396ca5e34f9a18722c84
wangtao <wangtao@ftxjoy.com>
2018-01-26 17:00:38 +0800

    Initial Commit

---
+++ SwizzledMethod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwizzledMethod
+//
+//  Created by 王滔 on 2018/1/26.
+//  Copyright © 2018年 Jone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwizzledMethod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwizzledMethod
+//
+//  Created by 王滔 on 2018/1/26.
+//  Copyright © 2018年 Jone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
